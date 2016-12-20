0c04c366cee0bb36be188437e91aa188094d0417
opme976 <t.nielsen976@gmail.com>
2016-12-20 13:16:09 +0100

    init commit

---
+++ 12.1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#**************
+#Scriptguden bag: Tobias Nielsen
+#Opgave: 12.1
+#Beskrivelse: for in do done
+
+
+for i in fem fire tre to en start; do echo $i; sleep 1; done
+#tæller ned fra 5 med en sleep timer på 1 sekundt
---
+++ 12.2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#**************
+#Scriptguden bag: Tobias Nielsen
+#Opgave: 12.2
+#Beskrivelse: for i in do done 2
+
+for i in /bin/ *.txt; do echo $i; sleep 1; done
